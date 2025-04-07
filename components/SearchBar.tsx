55ceca712fa2b195c4a96cba70d3a5e3cad55ee0
Kwaku28 <rekenburg777@gmail.com>
2025-04-07 11:19:16 +0000

    Create a search bar component to house the search

---
+++ components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { icons } from "@/constants/icons";
+import { View, Image, TextInput } from "react-native";
+
+interface props {
+  placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, onChangeText, onPress, value }: props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-3 text-white"
+        placeholderTextColor="#ab8bff"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
